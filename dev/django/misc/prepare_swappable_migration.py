513b718e5061fdb1f967fcbb9cc78e79b0e39f39
Ertuğrul Keremoğlu <ertugkeremoglu@gmail.com>
2024-05-30 14:08:20 +0300

    fix: the bodies of untyped functions are not checked

--- dev/django/misc/prepare_swappable_migration.py
+++ dev/django/misc/prepare_swappable_migration.py
@@ -8,17 +8,19 @@ from typing import (
 
 BASE_DIR = Path(__file__).resolve().parent.parent.parent.parent
 
-SWAPPABLE_SAMPLE_DIR = BASE_DIR / 'samples' / 'swappable'
+SWAPPABLE_SAMPLE_DIR = BASE_DIR / "samples" / "swappable"
 
-INITIAL_MIGRATION_FILE_PATH = SWAPPABLE_SAMPLE_DIR / 'sequences' / 'migrations' / '0001_initial.py'
+INITIAL_MIGRATION_FILE_PATH = (
+    SWAPPABLE_SAMPLE_DIR / "sequences" / "migrations" / "0001_initial.py"
+)
 
 
-def main():
+def main() -> None:
     migration_file_content = INITIAL_MIGRATION_FILE_PATH.read_text()
     migration_module = ast.parse(migration_file_content)
     migration_class_def: Optional[ast.ClassDef] = None
     for node in migration_module.body:
-        if isinstance(node, ast.ClassDef) and node.name == 'Migration':
+        if isinstance(node, ast.ClassDef) and node.name == "Migration":
             migration_class_def = node
             break
     if migration_class_def is None:
@@ -26,13 +28,13 @@ def main():
     for node in migration_class_def.body:
         if isinstance(node, ast.Assign):
             name = cast(ast.Name, node.targets[0])
-            if name.id == 'replaces':
+            if name.id == "replaces":
                 return
     replaces_node = ast.Assign(
         lineno=-1,
         targets=[
             ast.Name(
-                id='replaces',
+                id="replaces",
                 ctx=ast.Store(),
             ),
         ],
@@ -40,8 +42,8 @@ def main():
             elts=[
                 ast.Tuple(
                     elts=[
-                        ast.Constant(value='django_seq'),
-                        ast.Constant(value='0001_initial'),
+                        ast.Constant(value="django_seq"),
+                        ast.Constant(value="0001_initial"),
                     ],
                 ),
             ],
@@ -53,5 +55,5 @@ def main():
     )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
