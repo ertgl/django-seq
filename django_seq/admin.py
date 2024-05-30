b14226c8282f06d34cd1b768ac011247c7da18cb
Ertuğrul Keremoğlu <ertugkeremoglu@gmail.com>
2024-05-30 14:12:17 +0300

    fix: incompatible with return type

--- django_seq/admin.py
+++ django_seq/admin.py
@@ -13,19 +13,16 @@ from django_seq.models import Sequence
 from django_seq.utils import get_model_options
 
 
-__all__ = (
-    'SequenceAdmin',
-)
+__all__ = ("SequenceAdmin",)
 
 
 class SequenceAdmin(admin.ModelAdmin):
-
     model = Sequence
 
     model_options = get_model_options(Sequence)
 
     list_display = (
-        getattr(model_options.pk, 'name', 'pk'),
+        getattr(model_options.pk, "name", "pk"),
         Sequence.KEY_FIELD_NAME,
         Sequence.VALUE_FIELD_NAME,
         Sequence.CREATED_AT_FIELD_NAME,
@@ -35,7 +32,7 @@ class SequenceAdmin(admin.ModelAdmin):
     list_display_links = list_display
 
     search_fields = (
-        getattr(model_options.pk, 'name', 'pk'),
+        getattr(model_options.pk, "name", "pk"),
         Sequence.KEY_FIELD_NAME,
     )
 
@@ -48,7 +45,7 @@ class SequenceAdmin(admin.ModelAdmin):
         (
             None,
             {
-                'fields': (
+                "fields": (
                     Sequence.KEY_FIELD_NAME,
                     Sequence.VALUE_FIELD_NAME,
                 ),
@@ -56,14 +53,14 @@ class SequenceAdmin(admin.ModelAdmin):
         ),
     )
 
-    def formfield_for_dbfield(
+    def formfield_for_dbfield(  # type: ignore[override]
         self,
         db_field: models.Field,
         request: HttpRequest,
         **kwargs: Any,
     ) -> Optional[forms.Field]:
         if db_field.name == Sequence.KEY_FIELD_NAME:
-            kwargs['widget'] = widgets.AdminTextInputWidget
+            kwargs["widget"] = widgets.AdminTextInputWidget
         return super(SequenceAdmin, self).formfield_for_dbfield(
             db_field,
             request,
